d10229c3eaef3d7d062c0000a564bb6082d3f0ab
Crowdbotics <team@crowdbotics.com>
2021-02-16 13:37:05 +0000

    add package blueprint-react-native-tutorial-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Tutorial206104', human_name: 'Tutorial', access_route: 'Tutorial206104', icon: 'question-circle'},
 { name: 'NotificationList206076', human_name: 'Notification List', access_route: 'NotificationList206076'},
 { name: 'Settings206075', human_name: 'Settings', access_route: 'Settings206075'},
 { name: 'Settings206067', human_name: 'Settings', access_route: 'Settings206067'},
